bc46e9fa53fc3897db0a561cacbb7d62d28cae9a
Matthew Mahnke <mahnkem@msoe.edu>
2016-10-21 21:38:02 -0500

    Inti set prime factors

---
+++ inti_sets/inti_set.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import math
+
+
+def prime_factors(N):
+    pf = []
+    if N % 2 == 0:
+        pf.append(2)
+        while N % 2 == 0:
+            N = N >> 2
+    for x in range(3, math.floor(math.sqrt(N)), 2):
+        if N % x == 0:
+            pf.append(x)
+            while N % x == 0:
+                N = int(N / x)
+    if N > 2:
+        pf.append(N)
+    return pf
+
+
+print(prime_factors(10**12 - 139))
