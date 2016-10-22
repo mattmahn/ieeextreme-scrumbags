9c8b9fe3a6c2ebacdd602da81f1ac295e3e65573
Matthew Mahnke <mahnkem@msoe.edu>
2016-10-22 12:18:22 -0500

    fix da counting

--- counting_molecules/counting_molecules_matt.py
+++ counting_molecules/counting_molecules_matt.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
+from math import floor
+
+
 def main():
     vals = input().split()
     carbon = int(vals[0])
     hydrogen = int(vals[1])
     oxygen = int(vals[2])
 
-    carbon_diox = carbon - oxygen
+    carbon_diox = 0.25 * (2 * oxygen - hydrogen)
     glucose = (carbon - carbon_diox) / 6
     water = -carbon + carbon_diox + hydrogen / 2
 
-    print(water, carbon_diox, glucose)
+    print(floor(water), floor(carbon_diox), floor(glucose))
 
 
 main()
